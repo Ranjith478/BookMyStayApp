b30e81a182d8f497ee74691d3e733a4123791226
Ranjith478 <Ranjithsolomon18@gmail.com>
2026-03-11 20:53:26 +0530

    ranjith added uc2

---
+++ App/src/UseCase2RoomInitialization.java
@@ -0,0 +1,74 @@
+/**
+ * =====================================================
+ * MAIN CLASS – UseCase2RoomInitialization
+ * =====================================================
+ * Use Case 2: Basic Room Types & Static Availability
+ */
+
+abstract class Room {
+
+    protected int numberOfBeds;
+    protected int squareFeet;
+    protected double pricePerNight;
+
+    public Room(int numberOfBeds, int squareFeet, double pricePerNight) {
+        this.numberOfBeds = numberOfBeds;
+        this.squareFeet = squareFeet;
+        this.pricePerNight = pricePerNight;
+    }
+
+    public void displayRoomDetails() {
+        System.out.println("Beds: " + numberOfBeds);
+        System.out.println("Size: " + squareFeet + " sqft");
+        System.out.println("Price per night: " + pricePerNight);
+    }
+}
+
+class SingleRoom extends Room {
+
+    public SingleRoom() {
+        super(1, 250, 1500.0);
+    }
+}
+
+class DoubleRoom extends Room {
+
+    public DoubleRoom() {
+        super(2, 400, 2500.0);
+    }
+}
+
+class SuiteRoom extends Room {
+
+    public SuiteRoom() {
+        super(3, 750, 5000.0);
+    }
+}
+
+public class UseCase2RoomInitialization {
+
+    public static void main(String[] args) {
+
+        SingleRoom single = new SingleRoom();
+        DoubleRoom dbl = new DoubleRoom();
+        SuiteRoom suite = new SuiteRoom();
+
+        int singleAvailable = 5;
+        int doubleAvailable = 3;
+        int suiteAvailable = 2;
+
+        System.out.println("Single Room:");
+        single.displayRoomDetails();
+        System.out.println("Available: " + singleAvailable);
+        System.out.println();
+
+        System.out.println("Double Room:");
+        dbl.displayRoomDetails();
+        System.out.println("Available: " + doubleAvailable);
+        System.out.println();
+
+        System.out.println("Suite Room:");
+        suite.displayRoomDetails();
+        System.out.println("Available: " + suiteAvailable);
+    }
+}
